02585f034d8b460648dcb05210afd0ef22954cd2
gwt1014 <gwt1014@aliyun.com>
2018-07-27 15:50:41 +0800

    增加了链家二手房爬虫练习

---
+++ Lianjia2sf/Lianjia2sf/spiders/lianjia2sf.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from Lianjia2sf.items import Lianjia2SfItem
+
+
+class Lianjia2sfSpider(scrapy.Spider):
+    name = 'lianjia2sf'
+    allowed_domains = ['bj.lianjia.com']
+    start_urls = ['http://bj.lianjia.com/ershoufang/']
+
+    def parse(self, response):
+        # 获取每个城区的url，去掉燕郊和香河
+        district_list = response.xpath(
+            '//div[@class="sub_nav section_sub_nav"]/a/@href').extract()[:-2]
+        for district in district_list:
+            yield scrapy.Request('http://bj.lianjia.com' + district, callback=self.parse_getblockUrl)
+            # print('http://bj.lianjia.com' + districtUrl)
+            # print('#'*40)
+
+    def parse_getblockUrl(self, response):
+        block_list = response.xpath(
+            '//div[@class="sub_sub_nav section_sub_sub_nav"]/a/@href').extract()
+        for block in block_list:
+            yield scrapy.Request('http://bj.lianjia.com' + block + "ng1nb1/", callback=self.parse_gethouseUrl)
+            # print('http://bj.lianjia.com' + blockUrl)
+            # print('#'*40)
+
+    def parse_gethouseUrl(self, response):
+
+        houseUrl_list = response.xpath(
+            '//li[@class="clear LOGCLICKDATA"]/a/@href').extract()
+        for house in houseUrl_list:
+            # print(house)
+            # print('#'*40)
+            yield scrapy.Request(house, callback=self.parse_gethouseInfo)
+
+        # 获取下一页
+        # pagedata = json.loads(response.xpath('//div[@class="page-box house-lst-page-box"]/@page-data').extract()[0])
+        totalPage = json.loads(response.xpath(
+            '//div[@class="page-box house-lst-page-box"]/@page-data').extract()[0])["totalPage"]
+        curPage = json.loads(response.xpath(
+            '//div[@class="page-box house-lst-page-box"]/@page-data').extract()[0])["curPage"]
+
+        if curPage < totalPage:
+            next_page = curPage + 1
+            url = 'http://bj.lianjia.com' + \
+                response.xpath('//div[@class="page-box house-lst-page-box"]/@page-url').extract()[
+                    0].replace("{page}", str(next_page))
+            yield scrapy.Request(url, callback=self.parse_gethouseUrl)
+
+    def parse_gethouseInfo(self, response):
+        item = Lianjia2SfItem()
+
+        item['houseID'] = response.xpath(
+            '//div[@class="houseRecord"]/span[@class="info"]/text()').extract()[0]
+
+        item['title'] = response.xpath(
+            '//div[@class="title"]/h1/text()').extract()[0]
+
+        item['district'] = response.xpath(
+            '//span[@class="info"]/a[1]/text()').extract()[0]
+
+        item['block'] = response.xpath(
+            '//span[@class="info"]/a[2]/text()').extract()[0]
+
+        item['communityName'] = response.xpath(
+            '/html/body/div[5]/div[2]/div[4]/div[1]/a[1]/text()').extract()[0]
+
+        item['totlePrice'] = response.xpath(
+            '//span[@class="total"]/text()').extract()[0]
+
+        item['unitPrice'] = response.xpath(
+            '//span[@class="unitPriceValue"]/text()').extract()[0]
+
+        item['houseLink'] = response.xpath(
+            '//head/link[@rel="canonical"]/@href').extract()[0]
+
+        item['roommainInfo'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[1]/text()').extract()[0]
+        item['floor'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[2]/text()').extract()[0]
+        item['builtArea'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[3]/text()').extract()[0]
+        item['roomType'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[4]/text()').extract()[0]
+        item['useArea'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[5]/text()').extract()[0]
+        item['builtType'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[6]/text()').extract()[0]
+        item['orientation'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[7]/text()').extract()[0]
+        item['built'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[8]/text()').extract()[0]
+        item['decoration'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[9]/text()').extract()[0]
+        item['liftType'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[10]/text()').extract()[0]
+        item['heatingMode'] = response.xpath(
+            '///div[@class="base"]/div[@class="content"]/ul/li[11]/text()').extract()[0]
+        item['lift'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[12]/text()').extract()[0]
+        item['propertyRights'] = response.xpath(
+            '//div[@class="base"]/div[@class="content"]/ul/li[13]/text()').extract()[0]
+
+        item['houseType'] = response.xpath(
+            '//div[@class="transaction"]/div[@class="content"]/ul/li[4]/span[2]/text()').extract()[0]
+
+        yield item
+        # print(response.xpath('/html/body/div[5]/div[2]/div[4]/div[1]/a[1]/text()').extract()[0]
--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # scrapy爬虫框架应用
 
+#### 0. scrapy在windows环境下的安装，起手劝退！
+
+
+
+
+
+
+
+
+
+
+
 #### 1. 打开cmd 在要建立爬虫程序的目录下输入
 
 scrapy startproject xxxxx
@@ -14,10 +26,13 @@ scrapy genspider xxx "http://www.xxxx.com"
 
 3.2 编写spiders/xxx.py 编写爬虫文件，处理请求和响应，提取数据
 
+​	from xxx.items import xxxItem
+
 - yield item
-- yield scrapy.Resquest(url,callback = self.parse
+- yield scrapy.Resquest(url,callback = self.parse)
 
 3.3 编写pipelines.py 编写管道文件，处理spider返回的item数据，基本就是存json，在这里改编码.encode("utf-8")
+
 
 3.4 编写settings.py **启动管道组件**，其他设置。所有header都在这里，User-agent和其他的不在一起
 
@@ -54,4 +69,25 @@ scrapy genspider xxx "http://www.xxxx.com"
 
 2. 编码的修改还是在最后pipeline中最后保存的时候进行设置，在腾讯HR的里面报错了
 
-   ​
+## 3. 下载图片或媒体文件
+1. pipelines 
+    导入
+    import scrapy
+    from scrapy.pipelines.images import ImagesPipeline
+    from settings import IMAGES_STORE as images_store
+    import os #为了重命名用
+    保存
+    class xxxPipeline(ImagesPipeline)
+        #保存
+        def get_media_requests(self,item,info):
+            image_link = item['imagelink']
+            yield scrapy.Request(image_link)
+        #文件改名
+        def item_completed(self, results,item ,info ):
+            #取出图片路径
+            image_path = [x['path'] for ok ,x in results if ok]
+            #重命名
+            os.rename(images_store + image_path[0] ,images_store + item['nickname'] + ".jpg")
+
+2. setings 要增加参数
+    IMAGES_STORE = '绝对路径'
